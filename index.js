44790ed119bc9ff4c5f677bbe12f9402a0481162
Seohoo_Jang <jjs2961@naver.com>
2025-05-22 16:09:42 +0900

    게임 검색 기능 추가

--- index.js
+++ index.js
@@ -74,6 +74,23 @@ app.get('/api/gameDetailImg', async (req, res) => {
 });
 
 
+app.get('/api/gameSearch', async (req, res) => {
+    const search = req.query.search
+    
+
+  try {
+    const { data } = await axios.get(
+      `https://api.rawg.io/api/games?key=${API_KEY}&search=${search}&page_size=50`
+    );
+    res.json(data);
+    console.log(data)
+  } catch (error) {
+    console.error('DNF API 요청 실패:', error);
+    res.status(500).json({ error: 'DNF API 호출 실패' });
+  }14889810
+});
+
+
 app.listen(PORT, () => {
   console.log(`🔐 서버 실행 중: http://localhost:${PORT}`);
 });
